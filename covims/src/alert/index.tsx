f61b2c74b4b384d56cdc301af4b71a10137725ec
Mark Roberts <mark.roberts@linux-source.co.uk>
2019-12-04 15:32:15 +0000

    Font Awesome Icons

--- covims/src/alert/index.tsx
+++ covims/src/alert/index.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import {connect, ConnectedProps} from 'react-redux';
 import {filterToggle} from "../actions";
 import {IAlert} from "../reducers/alerts";
+import {FontAwesomeIcon as Icon} from '@fortawesome/react-fontawesome'
+import {
+    faCarCrash, faCarSide, faTruckPickup, faTimesCircle, faTrafficLight, faSmog
+}
+    from
+        '@fortawesome/free-solid-svg-icons'
+
 
 interface IProps {
     alerts?: object[]
@@ -12,9 +19,10 @@ interface IProps {
 type MyProps = PropsFromRedux & IProps;
 
 const filteredAlerts = (alerts: IAlert[], filters: string[], areas: string[]) => {
+    let sorted = alerts.slice().sort((a: any, b: any) => (b.time - a.time));
     return filters.length ?
-        alerts.filter((a: IAlert) => (filters.indexOf(a.type) >= 0)) :
-        alerts;
+        sorted.filter((a: IAlert) => (filters.indexOf(a.type) >= 0)) :
+        sorted;
 };
 
 interface IProps {
@@ -56,7 +64,7 @@ function timeDifference(t1: Date, t2: Date): string {
         let y: Date = new Date(t1);
         y.setDate(t1.getDate() - 1);
         const yesterday = y.toDateString();
-        console.log( 'Today:' + today + ' Yesterday:' + yesterday);
+        console.log('Today:' + today + ' Yesterday:' + yesterday);
         if (today === t2.toDateString()) {
             // today
             ds = 'Today';
@@ -64,9 +72,9 @@ function timeDifference(t1: Date, t2: Date): string {
             // yesterday
             ds = 'Yesterday'
         } else {
-            ds = t2.toLocaleDateString('en', { 'month': 'short', 'day': 'numeric'});
+            ds = t2.toLocaleDateString('en', {'month': 'short', 'day': 'numeric'});
         }
-        ds += ' ' + t2.toLocaleTimeString('en', { 'hour12': false, 'hour': '2-digit', 'minute': '2-digit'} );
+        ds += ' ' + t2.toLocaleTimeString('en', {'hour12': false, 'hour': '2-digit', 'minute': '2-digit'});
     }
     return ds;
 
@@ -108,16 +116,25 @@ const mapDispatch = {
 
 type PropsFromRedux = ConnectedProps<typeof connector>
 
+type MyOption = { label: string, value: number }
+
+function FilterButton(props: any) {
+    let selected = props.filter.indexOf(props.id);
+    // @ts-ignore
+    return <button className={'filter' + ((selected >= 0) ? ' selected' : '')}
+                   id={props.id}
+                   onClick={() => props.filterToggle(props.id)}
+    >
+        <Icon icon={props.icon} />
+        <div>{props.children}</div>
+    </button>
+}
+
 class AlertList extends React.Component<IProps, IState> {
 
-    FilterButton(id: string, text: string) {
-        let selected = this.props.filter.indexOf(id);
-        // @ts-ignore
-        return <button className={'filter' + ((selected >= 0) ? ' selected' : '')}
-                       id={id}
-                       onClick={() => this.props.filterToggle(id)}
-        >{text}
-        </button>
+    doChange = (e: ChangeEvent) => {
+        e.persist();
+        console.log(e)
     }
 
     render() {
@@ -130,19 +147,41 @@ class AlertList extends React.Component<IProps, IState> {
             <h2>Filters</h2>
             <div className='well'>
                 <div className='filter-row'>
-                    {this.FilterButton('broken', 'Broken Down')}
-                    {this.FilterButton('collision', 'Collision')}
-                    {this.FilterButton('lights', 'Faulty Traffic Light')}
+                    <FilterButton id='broken' icon={faTruckPickup}
+                                  filter={this.props.filter} filterToggle={this.props.filterToggle}>
+                        Broken Down
+                    </FilterButton>
+                    <FilterButton id='collision' icon={faCarCrash}
+                                  filter={this.props.filter} filterToggle={this.props.filterToggle}>
+                        Collision
+                    </FilterButton>
+                    <FilterButton id='lights' icon={faTrafficLight}
+                                  filter={this.props.filter} filterToggle={this.props.filterToggle}>
+                        Faulty Traffic Lights
+                    </FilterButton>
                 </div>
                 <div className='filter-row'>
-                    {this.FilterButton('closure', 'Road Closure')}
-                    {this.FilterButton('hazard', 'Hazards')}
-                    {this.FilterButton('traffic', 'Traffic')}
+                    <FilterButton id='closure' icon={faTimesCircle}
+                                  filter={this.props.filter} filterToggle={this.props.filterToggle}>
+                        Road Closure
+                    </FilterButton>
+                    <FilterButton id='hazard' icon={faSmog}
+                                  filter={this.props.filter} filterToggle={this.props.filterToggle}>
+                        Hazards
+                    </FilterButton>
+                    <FilterButton id='traffic' icon={faCarSide}
+                                  filter={this.props.filter} filterToggle={this.props.filterToggle}>
+                        Traffic
+                    </FilterButton>
                 </div>
             </div>
             <div className='well alerts'>
                 <h3>{alertTitle}</h3>
-                <p>Sorted by:</p>
+                <p>Sorted by: <select onChange={this.doChange} id='alert-sort'>
+                    <option>Most Recent</option>
+                    <option>Oldest</option>
+                </select>
+                </p>
                 <div className='alert-list'>
                     {alerts && alerts.map((a: any) => (
                         <Alert key={a.key} title={a.title} text={a.text} time={a.time}/>))}
